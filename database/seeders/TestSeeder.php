652e19502e0e3018c682891594e332b1ec5007b2
Dominik Zarsky <dominik.zarsky@gmail.com>
2021-04-14 09:13:57 +0200

    fix constraint problems on migrations and seeders

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -13,17 +13,22 @@ class TestSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(3)->create();
-        \App\Models\Book::factory(3)->create();
-        \App\Models\Author::factory(3)->create();
+        \App\Models\User::factory(1)->admin()->create([
+            'email' => 'admin@admin.com'
+        ]);
+        \App\Models\User::factory(20)->create();
         \App\Models\Category::factory(3)->create();
-        \App\Models\Cart::factory(3)->create();
-        \App\Models\CartItem::factory(3)->create();
-        \App\Models\ContactForm::factory(3)->create();
+        \App\Models\Book::factory(100)->create([
+            'category_id' => rand(1,3)
+        ]);
+        \App\Models\Author::factory(3)->create();
+        //\App\Models\Cart::factory(3)->create();
+        //\App\Models\CartItem::factory(3)->create();
+        //\App\Models\ContactForm::factory(3)->create();
         \App\Models\Customer::factory(3)->create();
-        \App\Models\Order::factory(3)->create();
-        \App\Models\OrderItem::factory(3)->create();
-        \App\Models\OrderReturn::factory(3)->create();
+        //\App\Models\Order::factory(3)->create();
+        //\App\Models\OrderItem::factory(3)->create();
+        //\App\Models\OrderReturn::factory(3)->create();
         \App\Models\PaymentMethod::factory(3)->create();
         \App\Models\ShippingMethod::factory(3)->create();
     }
