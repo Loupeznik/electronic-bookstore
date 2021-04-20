f0f8fa588a1a1521d310d249098f542fe7dd7eea
Dominik Zarsky <dominik.zarsky@gmail.com>
2021-04-20 13:14:44 +0200

    rework contact form controller

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -7,40 +7,55 @@ use App\Models\ContactForm;
 
 class ContactFormController extends Controller
 {
-    // Contact form page
+    /**
+     * Show list of pending contact forms.
+     * 
+     */
     public function index()
     {
-        return view('contact');
-    }
-
-    // Store created contact form
-    public function store(Request $request)
-    {
-        ContactForm::create($this->validateInput($request));
-
-        return redirect('/contact')->with('status', 'success');
-    }
-
-    // List all contact forms for an admin
-    public function list()
-    {
-        $forms = ContactForm::get();
+        $forms = ContactForm::where('status', 0)->get();
 
         return view('admin.contact.index', compact('forms'));
     }
 
-    // Show contact form details
-    public function show($id)
+    /**
+     * Show form to create a new contact form
+     */
+    public function create()
     {
-        $form = ContactForm::find($id);
-
-        return view ('admin.contact.detail', compact('form'));
+        return view('contact');
     }
 
-    // Delete contact form
-    public function destroy(ContactForm $form)
+    /**
+     * Store created contact form.
+     * 
+     * @param \Illuminate\Http\Request $request
+     */
+    public function store(Request $request)
     {
-        $form->delete();
+        ContactForm::create($this->validateInput($request));
+
+        return redirect('/contact')->with('status', 'success')->with('message', 'Contact form has been saved');
+    }
+
+    /**
+     * Show contact form details
+     * 
+     * @param \App\Models\ContactForm $contact
+     */
+    public function show(ContactForm $contact)
+    {
+        return view('admin.contact.detail', compact('contact'));
+    }
+
+    /**
+     * Delete a contact form
+     * 
+     * @param \App\Models\ContactForm $contact
+     */
+    public function destroy(ContactForm $contact)
+    {
+        $contact->delete();
 
         return redirect('/admin/contact')->with('status', 'success')->with('message', 'Contact form deleted');
     }
@@ -49,7 +64,7 @@ class ContactFormController extends Controller
     {
         return $input->validate([
             'name' => ['required', 'max:50', 'min:2'],
-            'email' => ['max:100', 'nullable'],
+            'email' => ['required', 'max:100'],
             'content' => ['required', 'min:10', 'max:1000'],
         ]);
     }
