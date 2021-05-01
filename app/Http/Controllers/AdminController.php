8b2f4058249c47714a34ef3173f8bc8d0928c591
Dominik Zarsky <43666286+Loupeznik@users.noreply.github.com>
2021-05-01 12:19:40 +0200

    update queries to fetch only pending records

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,9 +13,9 @@ class AdminController extends Controller
     public function index()
     {
         $orders = Order::latest()->take(10)->get();
-        $assignedOrders = Order::where('assignee_id', Auth::user()->id)->latest()->take(10)->get();
-        $assignedRefunds = OrderReturn::where('assignee_id', Auth::user()->id)->latest()->take(10)->get();
-        $assignedConForms = ContactForm::where('assignee_id', Auth::user()->id)->latest()->take(10)->get();
+        $assignedOrders = Order::where('assignee_id', Auth::user()->id)->where('status', '!=', 2)->where('status', '!=', 3)->latest()->take(10)->get();
+        $assignedRefunds = OrderReturn::where('assignee_id', Auth::user()->id)->where('status', '!=', 2)->latest()->take(10)->get();
+        $assignedConForms = ContactForm::where('assignee_id', Auth::user()->id)->where('status', 0)->latest()->take(10)->get();
 
         return view('dashboard', compact(['orders', 'assignedOrders', 'assignedRefunds', 'assignedConForms']));
     }
